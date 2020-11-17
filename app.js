7a6937963f31025351a5e3281ef4d01ae4c42ff6
李小明 <likeming@jasgroup.cn>
2020-11-17 14:06:18 +0800

    修改微信提示

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if (group != null) {
     let ui = document.createElement('p');
     ui.classList.add('tip');
-    ui.innerHTML = `请点右上角红色按钮下载安装，打开应用后点“队”按钮，输入队伍名称：<br><b>${group}</b><br>加入队伍。`;
+    ui.innerHTML = `请点右上角红色按钮下载安装，打开应用后点“队”按钮，输入队伍名称：<br><b>${group}</b>`;
     let article = document.body.querySelector('article#outmap > article');
     article.insertBefore(
       ui,
@@ -54,8 +54,8 @@ window.addEventListener('DOMContentLoaded', () => {
         <article>
           <h3>感谢你的信任</h3>
           <p>非微信官方网页——没给微信交钱的网页。</p>
-          <p>纵使你选择了继续访问，但微信还是没打算放弃拦路！我的功能已被阉割，无法正常显示。</p>
-          <p>请点一下白色方框，复制网址后到浏览器中粘贴打开，谢谢！</p>
+          <p>纵使你选择了继续访问，微信还是没打算放弃拦路打劫！网页无法正常显示……</p>
+          <p>请点一下白色方框，复制网址后到浏览器应用中粘贴打开，谢谢！</p>
           <p>
             <textarea>${window.location.href}</textarea>
           </p>
